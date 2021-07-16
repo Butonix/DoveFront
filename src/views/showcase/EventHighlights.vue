4614212fcf3db3129a15aff7ba3910a241e7260b
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-16 20:41:15 +0545

    Used top 3 approved events as highlights

--- src/views/showcase/EventHighlights.vue
+++ src/views/showcase/EventHighlights.vue
@@ -1,60 +1,56 @@
 <template>
-	<div class="events-highlights-wrapper">
+	<div class="highlights-wrapper mx-1">
 		<v-carousel
 			cycle
 			:show-arrows="false"
 			hide-delimiters
 			reverse-transition="fade-transition"
 			transition="fade-transition"
-			style="max-height: 280px;"
+			class="highlights-carousel mx-auto"
 		>
 			<v-carousel-item
-				v-for="(event, index) in events"
+				v-for="(event, index) in topThreeEvents"
 				:key="index"
 			>
 				<v-card
-					flat
 					class="mx-auto overflow-hidden"
-					max-width="1000"
-					height="100%"
 					color="transparent"
+					height="100%"
+					max-width="800"
+					flat
 				>
-					<v-list max-width="800"
+					<v-list
 						class="mx-auto"
-						height="100%"
 						style="position: relative;"
 						color="transparent"
 					>
 						<v-list-item-avatar
-							id="date-avatar"
-							color="#e88c04"
-							class="elevation-12"
+							class="date-avatar elevation-12"
 							:class="($vuetify.breakpoint.width < 400) ? 'mx-auto': ''"
 							size="120"
-							style="border: 2px solid white; position: absolute; top: 0; left: 5%; z-index: 2"
 						>
 							<v-row no-gutters
 								justify="center" align="center"
 							>
 								<v-col cols="12">
 									<h1 class="text-center">
-										{{ event.start_date.day }}<sup>{{
-											getSuperScript(event.start_date.day)
+										{{ $moment(event.start_date).format('DD') }}<sup>{{
+											getSuperScript($moment(event.start_date).format('D'))
 										}}</sup>
 									</h1>
 								</v-col>
 								<v-col cols="12"
-									class="month-column elevation-4 py-2"
+									class="month-column py-2"
 								>
 									<h2 class="text-center">
-										{{ event.start_date.month }}
+										{{ $moment(event.start_date).format('MMMM') }}
 									</h2>
 								</v-col>
 							</v-row>
 						</v-list-item-avatar>
 						<v-fade-transition>
 							<v-list-item-content
-								style="position: absolute; top: 70px; padding: 70px 20px 20px 20px; background: darkslategrey; border-radius: 4px;"
+								class="highlights-content"
 							>
 								<v-list-item-title
 									class="py-2"
@@ -70,28 +66,29 @@
 									<u>{{ event.title }}</u>
 								</v-list-item-title>
 								<v-list-item-subtitle v-show="$vuetify.breakpoint.smAndUp"
-									class="d-flex justify-space-between align-center"
+									class="d-flex justify-space-between align-center flex-wrap"
 								>
-									<p class="subtitle-2 ma-0 pa-0 text-start">
+									<p class="subtitle-2 ma-0 text-start">
 										<span><v-icon>mdi-sort-clock-descending</v-icon></span>
-										Friday 5:00 AM
+										{{ $moment(event.start_date).format('LLLL').split(',')[0] }}
+										{{ event.time_of_day }}
 									</p>
-									<p class="subtitle-2 ma-0 pa-0">
+									<p class="subtitle-2 ma-0">
 										<v-icon>mdi-sort-clock-ascending</v-icon>
-										Friday 8:00 PM
+										{{ $moment(event.start_date).add(event.duration, 'days').format('LLLL').split(',')[0] }}
 									</p>
 								</v-list-item-subtitle>
 								<v-list-item-subtitle
 									v-show="$vuetify.breakpoint.smAndUp"
-									class="d-flex justify-space-between align-center"
+									class="d-flex justify-space-between align-center flex-wrap"
 								>
-									<p class="ma-0 pa-0 subtitle-2 text-start">
+									<p class="ma-0 subtitle-2 text-start">
 										<v-icon>mdi-home</v-icon>
-										Deep Housing
+										{{ event.venue }}
 									</p>
-									<p class="subtitle-2 ma-0 pa-0">
+									<p class="subtitle-2 ma-0">
 										<v-icon>mdi-google-maps</v-icon>
-										Lamachaur-16, Pokhara
+										{{ getMunicipalityOrVdcWard(event) }}
 									</p>
 								</v-list-item-subtitle>
 							</v-list-item-content>
@@ -111,20 +108,54 @@
 	</div>
 </template>
 <script>
+import {mapGetters} from "vuex";
+
 export default {
 	name: "EventHighlights",
 	data: () => ({
-		events: [],
+		loading: null,
 		colors: [
 			"grey darken-3",
 			"#1F7087",
 			"#385F73",
 		],
 	}),
-	created() {
-		this.initialize();
+	computed: {
+		...mapGetters({
+			events: "event/list"
+		}),
+		topThreeEvents() {
+			if (this.events) {
+				if (this.events.results) {
+					if (this.events.count > 3) {
+						return [
+							this.events.results[0],
+							this.events.results[1],
+							this.events.results[2],
+						]
+					} else return this.events.results
+				} return []
+			} return []
+		}
+	},
+	async created() {
+		await this.init()
 	},
 	methods: {
+		async init() {
+			if (!this.events.length) {
+				this.loading = true
+				await this.$store.dispatch("event/filter", {is_approved: true})
+				this.loading = false
+			}
+		},
+		getMunicipalityOrVdcWard(event) {
+			if (event.municipality_ward) {
+				return `${event.municipality_ward.name}, ${event.municipality.name}`
+			} else if (event.vdc_ward) {
+				return `${event.vdc_ward.name}, ${event.vdc.name}`
+			} else return ""
+		},
 		getSuperScript(day) {
 			const rem = day % 10
 			if ( rem === 1) {
@@ -136,127 +167,36 @@ export default {
 			} else {
 				return "th"
 			}
-		},
-		initialize() {
-			const now = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "")
-			this.events = [
-				{
-					id: 1,
-					title: "Guru Purnima 2020",
-					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-						"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut " +
-						"enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-						"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
-						"in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
-						"sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
-						"mollit anim id est laborum.",
-					country: "Nepal",
-					province: "Gandaki Pradesh",
-					district: "Kaski",
-					municipality: "Pokhara",
-					municipality_ward: "Lamachaur",
-					vdc: null,
-					vdc_ward: null,
-					organizer: "Pokhara Kendra Branch",
-					venue: "Deep Housing, Bagar",
-					contacts: [9845689652, 6158965],
-					start_date: {
-						day: 25,
-						month: "Feb"
-					},
-					duration: 5,
-					timeOfDay: "10AM - 5PM",
-					is_main: true,
-					created_by: "kiran589",
-					created_at: now,
-					updated_by: "bot25",
-					updated_at: now,
-					banner:
-						"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/ED4B1180197DC35F40612607655B3DC0B5CFD688690B99B39B758927373D4C50"
-				},
-				{
-					id: 2,
-					title: "After Lockdown Gathering",
-					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-						"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut " +
-						"enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-						"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
-						"in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
-						"sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
-						"mollit anim id est laborum.",
-					country: "Nepal",
-					province: "Gandaki Pradesh",
-					district: "Kaski",
-					municipality: "Pokhara",
-					municipality_ward: "Lamachaur",
-					vdc: null,
-					vdc_ward: null,
-					is_main: true,
-					created_by: "bot25",
-					created_at: now,
-					updated_by: "bot25",
-					updated_at: now,
-					venue: "Deep Housing",
-					organizer: "Pokhara Kendra Branch",
-					contacts: [9845689652, 6158965],
-					start_date: {
-						day: 1,
-						month: "Mar"
-					},
-					duration: 5,
-					timeOfDay: "10AM - 5PM",
-					banner:
-						"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/ED4B1180197DC35F40612607655B3DC0B5CFD688690B99B39B758927373D4C50"
-				},
-				{
-					id: 3,
-					title: "Hungama Event",
-					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-						"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut " +
-						"enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-						"aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
-						"in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
-						"sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
-						"mollit anim id est laborum.",
-					country: "Nepal",
-					province: "Bagmati Pradesh",
-					district: "Kathmandu",
-					municipality: "Kathmandu",
-					municipality_ward: "Maitighar",
-					vdc: null,
-					vdc_ward: null,
-					is_main: true,
-					created_by: "bot25",
-					created_at: now,
-					updated_by: "bot25",
-					updated_at: now,
-					venue: "Maiti Ghar Mandala",
-					organizer: "Pokhara Kendra Branch",
-					contacts: [9845689652, 6158965],
-					start_date: {
-						day: 30,
-						month: "Jun"
-					},
-					duration: 2,
-					timeOfDay: "10AM - 5PM",
-					banner:
-						"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/ED4B1180197DC35F40612607655B3DC0B5CFD688690B99B39B758927373D4C50"
-				},
-			]
 		}
 	}
 }
 </script>
 <style lang="sass" scoped>
-.events-highlights-wrapper
-	margin-top: -170px
-	padding: 10px
-.month-column
-	background: rgba(0,0,0,.15)
+.date-avatar
+	border: 2px solid white !important
+	background: rgb(232 140 4 / 1%)
+	position: absolute
+	top: 0
+	left: 5%
+	z-index: 2
+.highlights-wrapper
+	margin-top: -20vh
+.highlights-carousel
+	max-width: 1000px
+	max-height: 290px
+	border-radius: 4px
+.highlights-content
+	position: absolute
+	top: 70px
+	padding: 70px 20px 20px 20px
+	background: darkslategrey
+	border-radius: 4px
 .small-title
 	font-size: 22px
 	line-height: 22px
 .big-title
 	font-size: 26px
 	line-height: 26px
+sup
+	font-size: 16px
 </style>
