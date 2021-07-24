f45fe560fed31d4c751e6eef31d426922f4b9a52
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-24 22:29:11 +0545

    Added fb group component

---
+++ src/components/utils/FacebookGroup.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-card
+		max-width="300"
+	>
+		<div class="fb-group"
+			data-href="https://www.facebook.com/groups/2553480628235742" data-width="300"
+			data-show-social-context="true" data-show-metadata="false"
+		>
+			<blockquote cite="https://www.facebook.com/groups/2553480628235742"
+				class="fb-xfbml-parse-ignore"
+			>
+				Sachchai Kendra Nepal
+			</blockquote>
+		</div>
+	</v-card>
+</template>
+
+<script>
+export default {
+	name: "FacebookGroup",
+	props: {},
+	data: () => ({}),
+	computed: {},
+	methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
