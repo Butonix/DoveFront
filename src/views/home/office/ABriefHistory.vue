7be70ff11018bdf18393d0dbea80936aea83b13a
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-09-19 10:19:16 +0545

    Update brief history with imgs and icons

--- src/views/home/office/ABriefHistory.vue
+++ src/views/home/office/ABriefHistory.vue
@@ -30,16 +30,37 @@
 					:color="item.color"
 					dark
 				>
-					<v-card-title class="text-h5 d-flex">
+					<v-card-title class="text-h5 d-flex"
+						:class="item.color + ' darken-1'"
+					>
 						<div>
 							{{ item.title }}
 						</div>
 						<v-spacer />
-						<div>{{ item.opposite }}</div>
+						<v-chip :color="item.color"
+							class="font-weight-bold"
+						>
+							{{ item.opposite }}
+						</v-chip>
 					</v-card-title>
-					<v-card-text class="white text--primary">
-						{{ item.description }}
-					</v-card-text>
+					<v-divider />
+					<v-row class="ma-0 pa-0">
+						<v-col cols="8">
+							<v-card-text class="white--text">
+								{{ item.description }}
+							</v-card-text>
+						</v-col>
+						<v-col cols="4">
+							<v-card rounded="xl"
+								class="transparent"
+							>
+								<v-img
+									:height="item.imgHeight"
+									:src="item.img"
+								/>
+							</v-card>
+						</v-col>
+					</v-row>
 				</v-card>
 			</v-timeline-item>
 		</v-timeline>
@@ -54,66 +75,84 @@ export default {
 		items: [
 			{
 				color: "red lighten-2",
-				icon: "mdi-star",
+				icon: "mdi-emoticon-sick",
 				opposite: "2066",
 				title: "A Baby Got Sick",
-				description: "It was a very unfortunate situation. Our baby was always depressing. He was never in his one hundred percent health state, both mentally and physically. What was troubling him? We visited many doctors but there was no any solution to this strange syndrome"
+				description: "It was a very unfortunate situation. Our baby was always depressing. He was never in his one hundred percent health state, both mentally and physically. What was troubling him? We visited many doctors but there was no any solution to this strange syndrome",
+				img: "https://www.pedpartners.com/wp-content/uploads/Sick-Child-1024x684.jpg",
+				imgHeight: 160
 			},
 			{
-				color: "purple darken-1",
-				icon: "mdi-book-variant",
+				color: "deep-purple",
+				icon: "mdi-train-car",
 				opposite: "2067",
 				title: "A Journey of Hope",
-				description: "We heard about self healing in Delhi, India. After being fed up with regular attempts we thought we should give it a try and packed our bags for Delhi. We reached there, a completely peaceful environment. We heard from many peoples. How miserable their life was before and how satisfied they now are. Our baby was participating in multiple such healing activities and gradually his health was progressing."
+				description: "We heard about self healing in Delhi, India. After being fed up with regular attempts we thought we should give it a try and packed our bags for Delhi. We reached there, a completely peaceful environment. We heard from many peoples. How miserable their life was before and how satisfied they now are. Our baby was participating in multiple such healing activities and gradually his health was progressing.",
+				img: "https://inkedandabroad.com/wp-content/uploads/2019/06/airplane-taking-off.jpg",
+				imgHeight: 225
 			},
 			{
 				color: "green lighten-1",
-				icon: "mdi-airballoon",
+				icon: "mdi-star-face",
 				opposite: "2067",
 				title: "A Lesson Is Learnt",
-				description: "Baby is amazingly healing and progressively having good health. Knows about the power of the self satisfaction and faith in 'Ishwor'. One thing we got struck was the concept of one ture God. Peoples are often arguing about their own God's image and acting weirdly about their very existence and meaning of life. We were totally convinced that there is only one ture God. The God Almighty will shower up us as long as we have faith in him."
+				description: "Baby is amazingly healing and progressively having good health. Knows about the power of the self satisfaction and faith in 'Ishwor'. One thing we got struck was the concept of one ture God. Peoples are often arguing about their own God's image and acting weirdly about their very existence and meaning of life. We were totally convinced that there is only one ture God. The God Almighty will shower up us as long as we have faith in him.",
+				img: "https://cdn.gtricks.com/2019/09/5-book-reading-android-apps-to-read-and-manage-books-for-free.jpg",
+				imgHeight: 245
 			},
 			{
 				color: "indigo",
-				icon: "mdi-buffer",
+				icon: "mdi-stairs-up",
 				opposite: "2068",
 				title: "Tiny steps for a big leap",
-				description: "Remembering the God Almighty and performing Bhajans were daily activities now. Satsangs are now performed within family and closed circles. We highly felt that locals should be benifited too. So we started sharing our ideas, experiences about 'Sachchai' to the public."
+				description: "Remembering the God Almighty and performing Bhajans were daily activities now. Satsangs are now performed within family and closed circles. We highly felt that locals should be benifited too. So we started sharing our ideas, experiences about 'Sachchai' to the public.",
+				img: "https://miro.medium.com/max/14720/1*fnEy_jy0cHc28fEkazPMsA.jpeg",
+				imgHeight: 150
 			},
 			{
-				color: "blue darken-2",
-				icon: "mdi-buffer",
+				color: "blue",
+				icon: "mdi-account-group",
 				opposite: "2069",
 				title: "More Participation of Believers",
-				description: "Peoples are now participating in our Satsangs and Bhajans. We also share how our life is impacted by this belief system and get motivated among ourselves with our own stories. Peoples are more than happy to being heard and motivated."
+				description: "Peoples are now participating in our Satsangs and Bhajans. We also share how our life is impacted by this belief system and get motivated among ourselves with our own stories. Peoples are more than happy to being heard and motivated.",
+				img: require("@/assets/mass_clap.jpg"),
+				imgHeight: 150
 			},
 			{
-				color: "teal darken-2",
-				icon: "mdi-buffer",
+				color: "teal",
+				icon: "mdi-registered-trademark",
 				opposite: "2070",
 				title: "Registered Non Profit Religious Organization",
-				description: "With the growing number of peoples attention, critisims were also growing. We'd to be clear with our ambition among the society. We deeply thought it should be registered and have a peculiar boundary for the operations performed inside. We are totally responsible for our official works and will certaily answer your questions about it. But all other un-official arguments are void due to our legal registration."
+				description: "With the growing number of peoples attention, critisims were also growing. We'd to be clear with our ambition among the society. We deeply thought it should be registered and have a peculiar boundary for the operations performed inside. We are totally responsible for our official works and will certaily answer your questions about it. But all other un-official arguments are void due to our legal registration.",
+				img: require("@/assets/kendra_set.jpg"),
+				imgHeight: 220,
 			},
 			{
 				color: "orange",
-				icon: "mdi-buffer",
+				icon: "mdi-source-branch-check",
 				opposite: "2071",
 				title: "Sachchai Branches",
-				description: "After our legalization we'd an official building to perform our Satsangs, Bhajans, Gawahis & Bachans more officially. Peoples were coming from very far places. It was so difficult for them to attain our everydays programs. So we suggested them to perform Satsangs on their local peoples with their fellow believers. This idea lead us to Branch registrations under 'Ishworiya Marg Bhajan Mandal Sachchai Kendra Nepal'. Now we have hundreds of branches throughout the country and number of branches in aboard countries like Japan, Korea, Australia etc."
+				description: "After our legalization we'd an official building to perform our Satsangs, Bhajans, Gawahis & Bachans more officially. Peoples were coming from very far places. It was so difficult for them to attain our everydays programs. So we suggested them to perform Satsangs on their local peoples with their fellow believers. This idea lead us to Branch registrations under 'Ishworiya Marg Bhajan Mandal Sachchai Kendra Nepal'. Now we have hundreds of branches throughout the country and number of branches in aboard countries like Japan, Korea, Australia etc.",
+				img: require("@/assets/sacchai_banner.jpg"),
+				imgHeight: 280,
 			},
 			{
 				color: "pink lighten-1",
-				icon: "mdi-buffer",
+				icon: "mdi-human-handsup",
 				opposite: "2072",
-				title: "Leaders Helping Hands",
-				description: "Those who are constantly participating in Satsangs and have more knowledge about Sachchai are made as Branch Leaders so that they can work as a link between Kendra Team and local branches. This way we can easily communicate with local peoples and listen to their problems."
+				title: "Leaders as Helping Hands",
+				description: "Those who are constantly participating in Satsangs and have more knowledge about Sachchai are made as Branch Leaders so that they can work as a link between Kendra Team and local branches. This way we can easily communicate with local peoples and listen to their problems.",
+				img: "https://adfinity.co.za/wp-content/uploads/sites/53/2020/04/3-1024x666.png",
+				imgHeight: 220,
 			},
 			{
 				color: "red lighten-1",
-				icon: "mdi-buffer",
+				icon: "mdi-bird",
 				opposite: "2072",
 				title: "Flowing river",
-				description: "Peoples were more benefited, happy and motivated when connected with Sachchai. Local groupings and satsangs are being continuously performed locally even in these hard COVID situations."
+				description: "Peoples were more benefited, happy and motivated when connected with Sachchai. Local groupings and satsangs are being continuously performed locally even in these hard COVID situations.",
+				img: "https://media.giphy.com/media/VehoU0h2Rl8Gc/giphy.gif?cid=ecf05e47zmkw5471j1whg2g8frdt3ei56k5kbzynn1b7sw4k&rid=giphy.gif&ct=g",
+				imgHeight: 150
 			},
 		],
 	}),
