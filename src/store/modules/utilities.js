5e4eb1fc0d9e93b44d959914410d9bead6a56b17
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-09-20 13:29:57 +0545

    Utilities API connection

--- src/store/modules/utilities.js
+++ src/store/modules/utilities.js
@@ -2,12 +2,19 @@ import $api from "@/handler/axios";
 
 const state = {
 	services: [],
-	gallery: []
+	gallery: [],
+	feedbacks: {},
+	feedback: {},
+	feedbackType: "0",
+	feedbackErrors: {}
 }
 
 const getters = {
 	serviceList: state => state.services.results,
-	galleryList: state => state.gallery.results
+	galleryList: state => state.gallery.results,
+	feedbackList: state => state.feedbacks,
+	feedbackType: state => state.feedbackType,
+	feedbackPostErrors: state => state.feedbackErrors
 }
 
 const mutations = {
@@ -16,6 +23,15 @@ const mutations = {
 	},
 	SET_GALLERY_IMAGES(state, value) {
 		state.gallery = value
+	},
+	SET_FEEDBACKS(state, value){
+		state.feedbacks = value
+	},
+	SET_FEEDBACK_TYPE(state, value){
+		state.feedbackType = value
+	},
+	SET_FEEDBACK_ERRORS(state, value){
+		state.feedbackErrors = value
 	}
 }
 
@@ -33,6 +49,36 @@ const actions = {
 		try {
 			const res = await $api.get("showcase-gallery")
 			commit("SET_GALLERY_IMAGES", res)
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async fetchFeedbacks({commit}, payload) {
+		try {
+			const res = await $api.getWithPayload("feedback", payload)
+			console.log(res)
+			commit("SET_FEEDBACKS", res)
+			return true
+		} catch (e) {
+			return false
+		}
+	},
+	async postFeedback({commit}, formData) {
+		try {
+			await $api.post("feedback/", formData)
+			return true
+		} catch (e) {
+			const status = parseInt(e.response.status.toString())
+			if (status === 400) {
+				commit("SET_FEEDBACK_ERRORS", e.response.data)
+			}
+			return false
+		}
+	},
+	async deleteFeedback({}, id) {
+		try {
+			await $api.delete("feedback/" + id)
 			return true
 		} catch (e) {
 			return false
